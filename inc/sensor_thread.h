bea01bfb565313f8f963eaebb7ff543a935d98ba
Luis Duarte <luis.duarte@outlook.pt>
2025-01-13 00:42:12 +0000

    working on pthread_cond_wait for read buffer

--- inc/sensor_thread.h
+++ inc/sensor_thread.h
@@ -8,21 +8,23 @@
 
 #include "fifo_buffer.h"
 
-#define MAX_BUFFER_SIZE     15
 
 #define MAX_TEMP_SENSOR1_READS 25
 #define MAX_TEMP_SENSOR2_READS 30
 #define TEMP_SENSOR1_TIMING 3
 #define TEMP_SENSOR2_TIMING 5
 
+#define AVERAGE_MAX_READS 5
+
 #define MAX_HUMID_SENSOR1_READS 15
 #define MAX_HUMID_SENSOR2_READS 25
 #define HUMID_SENSORS_TIMING 8
 
 typedef struct {
+    int sensor_name;
     int sensor_reads;
     int sensor_timing;
-    float *buffer;
+    fifo_buffer_t *buffer;
 } sensor_info_t;
 
 /// @brief Sensor functions to be included in threads
@@ -36,10 +38,5 @@ void *read_buffer_data ( void *arg );
 /// @brief 
 /// @param seconds 
 void delay_seconds( int seconds );
-
-/// @brief 
-/// @param head 
-/// @param sensor_data 
-void write_to_buffer ( float *buffer, float sensor_data  );
 
 #endif // _SENSOR_THREAD_H
--- src/sensor_thread.c
+++ src/sensor_thread.c
@@ -8,34 +8,61 @@ void *read_sensor_data ( void *arg ) {
     sensor_info_t *sensor_info = (sensor_info_t *) arg;
     int reads = 0;
 
-    while ( reads < sensor_info->sensor_reads ) {
-        write_to_buffer( sensor_info->buffer, rand() );
+    for ( int i = 0; i < sensor_info->sensor_reads; i++ ) {
+        int data_for_buffer = rand() % 101;
+        add_to_buffer_tail( sensor_info->buffer, data_for_buffer );
         reads++;
         #ifdef DEBUG
-            printf(" Sensor reads: %d\n", reads);
-            printf("** Buffer Size %d \n", get_list_size( sensor_info->buffer ) ); 
-            read_buffer( sensor_info->buffer );       
+            printf("*****       Added to Sensor %d: %d \n", sensor_info->sensor_name, data_for_buffer);
+            read_buffer( sensor_info->buffer );
         #endif
-        
         delay_seconds( sensor_info->sensor_timing );
     }
+
+    printf("Sensor %d reads: %d\n", sensor_info->sensor_name, reads);
     pthread_exit(NULL);
 }
 
 void *read_buffer_data ( void *arg ) {
-   
+    int sum = 0;
+    float average = 0.0;
+
+    fifo_buffer_t *fifo = (fifo_buffer_t *) arg;
+    
+    while ( 1 ) {
+        pthread_mutex_lock(&fifo->mutex);
+
+        while ( fifo->size == 0 && !fifo->terminar ) {
+            pthread_cond_wait(&fifo->cond, &fifo->mutex);
+        }
+
+        if ( fifo->size == 0 && fifo->terminar ) {
+            pthread_mutex_unlock(&fifo->mutex);
+            break;
+        }
+
+        sum = 0;
+        average = 0.0;
+        int reads = fifo->size < AVERAGE_MAX_READS ? fifo->size : AVERAGE_MAX_READS;
+        for(int i = 0; i < reads; i++) {
+            sum += fifo->buffer[fifo->end - i];
+        }
+        average = sum / reads;
+        printf(" ** Average: %.2f\n\n", average);
+
+        pthread_mutex_unlock(&fifo->mutex);
+
+        if ( fifo->terminar ) {
+            break;
+        }
+        pthread_cond_wait(&fifo->cond, &fifo->mutex);
+    }
+    pthread_exit(NULL);
 }
 
 /* ********************************* */
 /*  ****  NON-THREAD FUNCTIONS  **** */
 /* ********************************* */
-void write_to_buffer ( float *buffer, float sensor_data ) {
-    if ( MAX_BUFFER_SIZE == get_list_size( buffer ) ) {          // If buffer is not full
-        printf("Buffer is full\n");
-        remove_from_buffer_head( buffer );       // Remove the oldest entry
-    }
-    add_to_buffer_tail( buffer, sensor_data );          // Add sensor data to the buffer
-}
 
 void delay_seconds(int seconds) {
     struct timespec ts;
